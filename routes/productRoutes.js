1f6415b861dcd30f722394f3e104f7671cf3f5e4
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-20 15:41:41 -0400

    added routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
