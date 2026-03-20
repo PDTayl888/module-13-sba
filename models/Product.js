146bf7cb99d22905a386fce01eb449dd94ec2a43
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-20 15:16:20 -0400

    edited product schema

--- models/Product.js
+++ models/Product.js
@@ -12,6 +12,35 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'price required'],
-        
+        validate: {
+            validator: function(value) {
+                return value > 0;
+            },
+            message: 'price must be greater than 0'
+        }
+    },
+    category: {
+        type: String, 
+        required: [true, 'product category required']
+    },
+    inStock: {
+        type: Boolean,
+        default: trusted
+    },
+    tags: {
+        type: Boolean,
+        default: true
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-})
+}); 
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
